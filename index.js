8a0ebb0fbac23683c81bee6205a2117707e34d0b
Jibin Babu <jibinbabu966@gmail.com>
2022-05-16 18:32:47 +0530

    deploy

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const connect = require("./src/configs/db");
 const tableController = require("./src/controllers/table.controller");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", tableController);
 
-app.listen(3030, async () => {
+app.listen(process.env.PORT || 3001, async () => {
   try {
     await connect();
     console.log("Listening on port 3030");
